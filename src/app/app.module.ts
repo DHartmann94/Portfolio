74df30c92efd5b43274b8071d09b2ff4b61cfab8
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-05-31 18:19:49 +0200

    introduction start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderMobileComponent } from './header-mobile/header-mobile.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderMobileComponent
+    HeaderMobileComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
