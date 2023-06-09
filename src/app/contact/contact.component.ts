8718928affa11ec6cc74551cb952128202beaa76
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-06-09 15:50:25 +0200

    send mail loader / real-time validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -17,6 +17,9 @@ export class ContactComponent {
   showErrorMessageMessage: boolean = false;
   showSendMailMessage: boolean = false;
 
+  /**
+   * These functions are for validating and sending the contact field. 
+   */
   async sendMail() {
     let nameField = this.nameField.nativeElement;
     let emailField = this.emailField.nativeElement;
@@ -25,7 +28,6 @@ export class ContactComponent {
     let formData = new FormData();
 
     this.disableInput(nameField, emailField, messageField, sendButton);
-    // animation
     this.prepareTransmitMail(formData, nameField, emailField, messageField);
     await this.transmitMail(formData);
     this.clearInput(nameField, emailField, messageField);
@@ -62,7 +64,6 @@ export class ContactComponent {
   }
 
   async transmitMail(formData: any) {
-    // https://daniel-hartmann.developerakademie.net/send_mail/send_mail.php
     try {
       await fetch('https://daniel-hartmann.developerakademie.net/send_mail/send_mail.php',
         {
@@ -79,6 +80,36 @@ export class ContactComponent {
     setTimeout(() => {
       this.showSendMailMessage = false;
     }, 3000);
+  }
+
+  /**
+   * These functions are real-time validation.
+   * @param inputType - name, email or message
+   */
+  blurVerify(inputType: string) {
+    (inputType === 'name' || inputType === 'message') ? this.validNameOrMessage(inputType) : this.validEmail(inputType);
+  }
+
+  validNameOrMessage(inputType: string) {
+    const inputField = (this as any)[inputType + 'Field'].nativeElement;
+    inputField.value.length > 0 ? this.inputValid(inputType) : this.inputInvalid(inputType);
+  }
+
+  validEmail(inputType: string) {
+    const inputField = (this as any)[inputType + 'Field'].nativeElement;
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputField.value) ? this.inputValid(inputType) : this.inputInvalid(inputType);
+  }
+
+  inputValid(inputType: string) {
+    let selectedInputField = 'showErrorMessage' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    (this as any)[`${selectedInputField}`] = false;
+    (this as any)[inputType + 'Field'].nativeElement.classList.add('valid-input');
+  }
+
+  inputInvalid(inputType: string) {
+    let selectedInputField = 'showErrorMessage' + inputType.charAt(0).toUpperCase() + inputType.slice(1);
+    (this as any)[inputType + 'Field'].nativeElement.classList.remove('valid-input');
+    (this as any)[`${selectedInputField}`] = true;
   }
 
 
